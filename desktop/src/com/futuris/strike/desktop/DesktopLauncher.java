84764f69a5b3b01febf99e4b564851af2820b8a0
silvia-odwyer <silviaodwyer119@yahoo.ie>
2017-11-30 23:54:45 +0000

    Added all build files, so that the game can compile and launch successfully.

---
+++ desktop/src/com/futuris/strike/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.futuris.strike.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.futuris.strike.FuturisStrikeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FuturisStrikeGame(), config);
+	}
+}
