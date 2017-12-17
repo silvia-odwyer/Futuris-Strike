105d5b0c1a5626e6e0d640195cc02aa07a4ff153
silvia-odwyer <silviaodwyer119@yahoo.ie>
2017-12-17 14:40:59 +0000

    Added the Home Screen, which provides a short introductory screen before the user enters the game.

--- core/src/com/futuris/strike/FuturisStrikeGame.java
+++ core/src/com/futuris/strike/FuturisStrikeGame.java
@@ -2,29 +2,32 @@ package com.futuris.strike;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import screens.GameScreen;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
+import screens.HomeScreen;
+
 
 public class FuturisStrikeGame extends Game {
 	public SpriteBatch batch;
-
+	public BitmapFont homeFont;
 	public int userPoints;
 	public int pcPoints;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		
+		homeFont = new BitmapFont(Gdx.files.internal("data/MediumFont/MediumEmulogic.fnt"));
 		userPoints = 0;
 		pcPoints = 0;
-		this.setScreen(new GameScreen(this));
+		this.setScreen(new HomeScreen(this));
 	}
 
 	@Override
---
+++ core/src/screens/HomeScreen.java
@@ -0,0 +1,68 @@
+package screens;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+
+import com.futuris.strike.FuturisStrikeGame;
+import com.badlogic.gdx.Screen;
+
+public class HomeScreen implements Screen {
+	private FuturisStrikeGame game;
+	
+	public HomeScreen(FuturisStrikeGame game) {
+		this.game = game;
+	}
+	
+	@Override 
+	public void show() {
+		
+	}
+	
+	public void handleInput() {
+		if(Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+			game.setScreen(new GameScreen(game));
+		}
+	}
+	
+	@Override 
+	public void render(float delta) {
+		handleInput();
+		
+		Gdx.gl.glClearColor(0,  0,  0,  0);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		game.batch.begin();
+		game.homeFont.draw(game.batch, "FUTURIS STRIKE", Gdx.graphics.getWidth() / 2 - 40, Gdx.graphics.getHeight() - 40);
+		game.batch.end();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		
+		
+	}
+	
+	
+	@Override
+	public void pause() {
+		
+		
+	}
+	
+	@Override
+	public void resume() {
+		
+		
+	}
+	
+	@Override 
+	public void hide() {
+		
+	}
+	
+	@Override 
+	public void dispose() {
+		
+		
+	}	
+}
